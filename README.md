f8720a7107b322e094dfbc58d1700db83f0ac57b
Shaun Morrison <shaun@picturesandwriting.com>
2018-05-16 19:55:27 +0100

    A compass that tells direction by sound
    Basic functionality of a compass that tells you direction with tones and pitches.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # Micro:bit Sound Compass
 
 #Aims of the App
+
 Help the user identify which direction they are facing using sound
+
+## Details and controls
+
+* Calibrate the compass (annoying, but it has to be done)
+* Rotate the Micro:bit and you will hear the pitch rises as you become close to NESW and then lowers as you get further away.
+* There is a different pitch for each of the four directions.
+* There is also the direction displayed for whichever of the four directions you are facing.
--- mb-app-sound-compass.py
+++ mb-app-sound-compass.py
@@ -1,17 +1,48 @@
 from microbit import*
+import music
+
+north = 0
+east = 90
+south = 180
+west = 270
+
+northPitch = 659 #E fifth
+eastPitch = 440 #A forth
+westPitch = 294 #D forth
+southPitch = 196 #G 3rd
 
 #start compass calibration
 compass.calibrate()
 
+
+def compassOutput(compassDir, directionDeg, pitch, displayChars):
+	global compassTotal
+	global pitchDuration
+	#North has to be treated differently due to being greater than 0 and less than 360
+	if directionDeg == 0:
+		if compassDir > 315:
+			pitchChange = pitch - (360 - compassDir)
+		else:
+			pitchChange = pitch - compassDir
+	else:
+		if compassDir > (directionDeg - 45) and compassDir <= directionDeg:
+			pitchChange = pitch - (directionDeg - compassDir)
+		else:
+			pitchChange = pitch - (compassDir - directionDeg)
+
+	display.show(displayChars)
+	music.pitch(pitchChange, 100)
+
+
 #MAIN APP LOOP
 while True:
-	sleep(100)
+	# sleep(100)
 	compassDir = compass.heading()
-	if (compassDir > 315 and compassDir <= 360) or (compassDir >= 0 and compassDir <= 45):
-		display.show('N')
+	if (compassDir > 315 or compassDir <= 45):
+		compassOutput(compassDir, north, northPitch, 'N')
 	elif compassDir > 45 and compassDir <= 135:
-		display.show('E')
+		compassOutput(compassDir, east, eastPitch, 'E')
 	elif compassDir > 135 and compassDir <= 225:
-		display.show('S')
+		compassOutput(compassDir, south, southPitch, 'S')
 	else:
-		display.show('W')
+		compassOutput(compassDir, west, westPitch, 'W')
